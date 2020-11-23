0354a8d05d66bc2d168a71ae7835f9aa4731fb49
Pyp <pierre.henrpi@gmail.com>
2020-11-23 15:52:56 +0100

    affichage de l'echiquier et des pions

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+# '*' pions noirs
+# 'o' pions blancs
+# '.' cae vide
+
 def affichage_plateau(state_plateau):
-    print("    "+str(state_plateau[0])+"    ")
-    print("  "+str(state_plateau[1])+"   "+str(state_plateau[2])+"  ")
-    print(str(state_plateau[3])+"   "+str(state_plateau[4])+"   "+str(state_plateau[5]))
-    print("  "+str(state_plateau[6])+"   "+str(state_plateau[7])+"  ")
-    print("    "+str(state_plateau[8])+"    ")
+    print("    "+str(state_plateau[2])+"    ")
+    print("  "+str(state_plateau[1])+"   "+str(state_plateau[3])+"  ")
+    print(str(state_plateau[0])+"   "+str(state_plateau[8])+"   "+str(state_plateau[4]))
+    print("  "+str(state_plateau[7])+"   "+str(state_plateau[5])+"  ")
+    print("    "+str(state_plateau[6])+"    ")
 
 if __name__ == "__main__":
-    state_plateau=[1,0,0,0,0,0,0,0,0]
+    state_plateau=['*','*','*','*','o','o','o','o','.']
     affichage_plateau(state_plateau)
