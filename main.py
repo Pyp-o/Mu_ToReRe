56b39288c96ad5950376d4c54a255295c1b60ecb
Pyp <pierre.henrpi@gmail.com>
2020-11-23 15:41:36 +0100

    affichage plateau

---
+++ main.py
@@ -0,0 +1,7 @@
+def affichage_plateau():
+    state_plateau=[1,0,0,0,0,0,0,0,0]
+    print("    "+str(state_plateau[0])+"    ")
+    print("  "+str(state_plateau[1])+"   "+str(state_plateau[2])+"  ")
+    print(str(state_plateau[3])+"   "+str(state_plateau[4])+"   "+str(state_plateau[5]))
+    print("  "+str(state_plateau[6])+"   "+str(state_plateau[7])+"  ")
+    print("    "+str(state_plateau[8])+"    ")
