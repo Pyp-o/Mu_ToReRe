8ee0599692781431836ee48047508d027bce9620
Pyp <pierre.henrpi@gmail.com>
2020-12-13 15:53:51 +0100

    save bug fixed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from plateau import *
 from agent import *
+from memToCSV import *
 
 # '*' pions noirs
 # 'o' pions blancs
@@ -28,7 +29,20 @@ def launch_game(plateau, agent_noir, agent_blanc):
     print("fin de partie")
     # On reinitialise la position des pions pour relancer une partie
     
-    
+    def load_agent(agent_noir):
+        try :
+            #on peut stocker la memoire d'etat sans traitement
+            agent_noir.mem_etat = load_mem("etat.csv")  # load etats
+
+            #on va devoir concatener la position et les probas en gardant la correspondance des indices
+            position = load_mem("position.csv")  # load actions
+            proba = load_mem("proba.csv")  # load probas
+
+            # permet d'aavoir la même longueur pour chaque mémoire chargée
+            if len(proba) != len(position):
+                position.append([])
+        except:
+            return 0
 
 
 if __name__ == "__main__":
@@ -41,5 +55,7 @@ if __name__ == "__main__":
         launch_game(plateau, agent_noir, agent_blanc)
         plateau.new_game(agent_noir, agent_blanc)
         nb_game += 1
-    
-    
+
+    save_mem(agent_noir.mem_actions, "mem_action")   #save actions et probas
+    save_mem(agent_noir.mem_etat, "mem_etat")          #save etat
+
--- memToCSV.py
+++ memToCSV.py
@@ -3,23 +3,28 @@ import csv
 import numpy as np
 from Bio import SeqIO
 
-def save_mem(array, text):
+def save_mem(array, text="mem_etat"):
+    #ici on ne stockera que la memoire des etats, des actions et des probas associées
     if text=="mem_action":
+        #on va séparer les actions des probabilités afin de faciliter l'exportation au format CSV
         position = []
         proba = []
         for i in range(0,len(array)):
             position.append(array[i][0])
             proba.append(array[i][1])
+        #on stocke les actions et les probas dans des fichiers CSV
         with open("position.csv", "w") as f:
             wr = csv.writer(f)
             wr.writerows(position)
         with open("proba.csv", "w") as f:
             wr = csv.writer(f)
             wr.writerows(proba)
+    #si l'on stocke l'etat dans un CSV
     elif text=="mem_etat":
         with open("etat.csv", "w") as f:
             wr = csv.writer(f)
             wr.writerows(array)
+
 
 def load_mem(file):
     tab = []
@@ -28,27 +33,35 @@ def load_mem(file):
         csv_reader = csv.reader(read_obj)
         # Pass reader object to list() to get a list of lists
         list_of_rows = list(csv_reader)
+
+        #lors de l'exportation au format CSV, des listes vides s'interposent entre chaque index, on s'en debarasse donc
         for i in range(len(list_of_rows)):
             if list_of_rows[i] != []:
                 tab.append(list_of_rows[i])
+
+        #au formati CSV, les valeurs sont stockées sous forme de string, on les caste pour obtenir le format voulu
         for i in range(0, len(tab)):
             for j in range(0, len(tab[i])):
                 if file == "proba.csv":
                     tab[i][j] = float(tab[i][j])
                 elif file == "position.csv":
                     tab[i][j] = int(tab[i][j])
+
         return tab
 
 
-mem_action = [[[8, -1, -1, 8], [0.5, 0.0, 0.0, 0.5]], [[-1, -1, -1, 4], [0.0, 0.0, 0.0, 1.0]], [[-1, -1, -1, 5], [0.0, 0.0, 0.0, 1.0]], [[8, -1, 8, 8], [0.3333333333333333, 0.0, 0.3333333333333333, 0.3333333333333333]], [[-1, -1, 3, -1], [0.0, 0.0, 1.0, 0.0]], [[-1, -1, 4, 4], [0.0, 0.0, 0.5, 0.5]]]
+"""mem_action = [[[8, -1, -1, 8], [0.5, 0.0, 0.0, 0.5]], [[-1, -1, -1, 4], [0.0, 0.0, 0.0, 1.0]], [[-1, -1, -1, 5], [0.0, 0.0, 0.0, 1.0]], [[8, -1, 8, 8], [0.3333333333333333, 0.0, 0.3333333333333333, 0.3333333333333333]], [[-1, -1, 3, -1], [0.0, 0.0, 1.0, 0.0]], [[-1, -1, 4, 4], [0.0, 0.0, 0.5, 0.5]]]
 mem_etat = [['*', '*', '*', '.', 'o', 'o', 'o', 'o', '*'], ['*', '*', '*', 'o', '*', 'o', 'o', 'o', '.'], ['*', '*', '*', 'o', '.', '*', 'o', 'o', 'o'], ['*', '*', '.', '*', 'o', '*', 'o', 'o', 'o'], ['*', '*', 'o', '.', 'o', '*', 'o', 'o', '*'], ['*', '*', '*', 'o', 'o', '*', 'o', 'o', '.'], ['*', '*', '*', 'o', '*', '.', 'o', 'o', 'o'], ['*', '*', '.', 'o', '*', 'o', 'o', 'o', '*'], ['*', '*', 'o', '*', '*', 'o', 'o', 'o', '.'], ['*', '*', 'o', '*', '.', '*', 'o', 'o', 'o'], ['.', '*', 'o', '*', 'o', '*', 'o', 'o', '*'], ['o', '*', 'o', '*', 'o', '*', 'o', '*', '.'], ['o', '*', 'o', '*', 'o', '*', '*', '.', 'o'], ['o', '*', 'o', '.', 'o', '*', '*', 'o', '*'], ['o', '*', 'o', 'o', '*', '.', '*', 'o', '*'], ['.', '*', '*', '*', 'o', 'o', 'o', 'o', '*'], ['o', '*', '*', '*', 'o', 'o', 'o', '*', '.'], ['*', '*', '*', '*', 'o', 'o', 'o', '.', 'o'], ['o', '*', '*', '.', 'o', '*', 'o', '*', 'o'], ['o', '*', '*', 'o', '*', '.', 'o', '*', 'o'], ['o', '*', '*', 'o', '*', 'o', '*', '.', 'o'], ['*', '.', '*', 'o', '*', 'o', '*', 'o', 'o'], ['*', 'o', '.', 'o', '*', 'o', '*', 'o', '*'], ['*', '*', 'o', 'o', '*', 'o', '*', 'o', '.'], ['*', '*', 'o', '*', '.', 'o', '*', 'o', 'o'], ['.', '*', 'o', '*', 'o', 'o', '*', 'o', '*'], ['o', '*', 'o', '*', 'o', 'o', '.', '*', '*'], ['*', '*', 'o', '*', 'o', '*', 'o', '.', 'o'], ['*', '.', 'o', '*', 'o', '*', 'o', 'o', '*'], ['*', 'o', '*', '*', 'o', '*', 'o', 'o', '.'], ['*', 'o', '*', '.', 'o', '*', 'o', 'o', '*'], ['*', 'o', '*', 'o', '*', '*', 'o', 'o', '.'], ['.', '*', '*', 'o', '*', '*', 'o', 'o', 'o'], ['*', '.', '*', '*', 'o', 'o', 'o', '*', 'o'], ['*', 'o', '*', '.', 'o', 'o', 'o', '*', '*'], ['*', 'o', '*', 'o', '*', 'o', 'o', '*', '.'], ['*', 'o', '*', 'o', '.', '*', 'o', '*', 'o'], ['*', 'o', '.', '*', 'o', '*', 'o', '*', 'o'], ['.', '*', 'o', '*', 'o', '*', 'o', '*', 'o'], ['o', '*', 'o', '*', 'o', '*', 'o', '.', '*'], ['o', '*', 'o', '*', 'o', '.', '*', 'o', '*'], ['o', '*', 'o', '*', '*', 'o', '*', 'o', '.'], ['*', '.', 'o', '*', '*', 'o', '*', 'o', 'o'], ['.', 'o', 'o', '*', '*', 'o', '*', 'o', '*'], ['o', 'o', 'o', '*', '*', 'o', '*', '*', '.'], ['*', 'o', 'o', '*', '*', 'o', '*', '.', 'o'], ['*', 'o', 'o', '*', '.', 'o', '*', 'o', '*'], ['*', 'o', 'o', '*', 'o', '*', '.', 'o', '*'], ['*', 'o', 'o', '*', 'o', '*', 'o', '*', '.'], ['*', 'o', '*', '.', 'o', '*', 'o', '*', 'o'], ['*', 'o', '.', 'o', 'o', '*', 'o', '*', '*'], ['*', '*', 'o', 'o', 'o', '*', 'o', '*', '.'], ['*', '*', 'o', 'o', 'o', '*', '*', '.', 'o'], ['.', '*', 'o', 'o', 'o', '*', '*', 'o', '*'], ['o', '*', 'o', 'o', 'o', '*', '*', '*', '.'], ['o', '.', '*', 'o', 'o', '*', '*', '*', 'o'], ['*', 'o', '*', 'o', 'o', '*', '*', '.', 'o'], ['*', 'o', '.', 'o', 'o', '*', '*', 'o', '*'], ['o', '*', '*', 'o', 'o', '*', '*', 'o', '.'], ['o', '*', '*', 'o', 'o', '*', '.', '*', 'o'], ['o', '*', '.', 'o', 'o', '*', 'o', '*', '*'], ['o', '*', 'o', '*', '*', '.', 'o', '*', 'o'], ['o', '*', 'o', '*', '*', 'o', '*', '.', 'o'], ['o', '*', 'o', '.', '*', 'o', '*', 'o', '*'], ['o', '*', '*', 'o', '*', 'o', '*', 'o', '.'], ['o', '*', '*', 'o', '*', 'o', '.', '*', 'o'], ['o', '*', '*', 'o', '.', '*', 'o', '*', 'o'], ['o', '*', '.', '*', 'o', '*', 'o', '*', 'o'], ['o', '*', 'o', '*', 'o', '.', 'o', '*', '*'], ['o', '*', 'o', '*', '*', 'o', 'o', '*', '.'], ['o', '*', '*', '.', '*', 'o', '*', 'o', 'o'], ['o', '*', '*', 'o', '.', 'o', '*', 'o', '*'], ['o', '*', '*', '*', 'o', 'o', '*', 'o', '.'], ['o', '*', '*', '*', 'o', 'o', '.', '*', 'o'], ['.', '*', '*', 'o', 'o', '*', 'o', 'o', '*'], ['o', '*', '*', 'o', 'o', '*', 'o', '*', '.'], ['o', '*', '*', 'o', '.', 'o', 'o', '*', '*'], ['o', '*', '*', '.', '*', 'o', 'o', '*', 'o']]
 
-save_mem(mem_action, "mem_action")
-save_mem(mem_etat, "mem_etat")
-position = load_mem("position.csv")
-proba = load_mem("proba.csv")
-etat = load_mem("etat.csv")
-print(position)
-print(proba)
-print(etat)
+save_mem(mem_action, "mem_action")   #save actions et probas
+save_mem(mem_etat, "mem_etat")          #save etat
+position = load_mem("position.csv")     #load actions 
+proba = load_mem("proba.csv")           #load probas
+etat = load_mem("etat.csv")             #load etats
+print("position", position)
+print("taille", len(position))
+print("proba", proba)
+print("taille", len(proba))
+print("etat", etat)
+print("taille", len(etat))"""
 
