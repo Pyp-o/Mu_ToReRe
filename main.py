6f767231ecdf8da6f0b6c6f846049871dda1c99a
Pyp <pierre.henrpi@gmail.com>
2020-12-14 12:00:25 +0100

    reward ok

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     #load_agent(agent_noir)
 
     nb_game = 0
-    while nb_game < 20 :
+    while nb_game < 100 :
         launch_game(plateau, agent_noir, agent_blanc)
         plateau.new_game(agent_noir, agent_blanc)
         nb_game += 1
